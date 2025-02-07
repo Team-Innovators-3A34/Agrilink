a35d9c62e64258daf1f1f185cda0710c0108b72d
Hama3004 <ghodbane.mohamed@esprit.tn>
2025-02-07 23:53:44 +0100

    Feat : Gestion des evenements

---
+++ src/Form/CategorieType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class CategorieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('nom', TextType::class, [
+            'label' => "Nom du categorie",
+            'required' => true,
+            'attr' => [
+                'class' => 'form-control',
+                'placeholder' => "Entrez le nom du categorie",
+            ],
+        ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Categorie::class,
+        ]);
+    }
+}
